d65a305d88ff4e4fc695c8b595211b2909e0081c
kaushalchauhan84850 <kaushalchauhan84850@gmail.com>
2026-07-14 17:09:29 +0530

    first

--- scripts/create-db.py
+++ scripts/create-db.py
@@ -1,29 +1,33 @@
-import pymysql
-
 import os
+import pymysql
 from dotenv import load_dotenv
-load_dotenv('/home/jenkins/.env')
+
+load_dotenv("/home/jenkins/.env")
+
 
 def create_database():
-    connection=pymysql.connect(
+    connection = pymysql.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
-        name=os.getenv("DB_NAME"),
         password=os.getenv("DB_PASSWORD"),
-        port=int(os.getenv("DB_PORT",3306))
+        port=int(os.getenv("DB_PORT", 3306))
     )
-    
+
     try:
         with connection.cursor() as cursor:
-            cursor.execute(f"CREATE DATABASE IF NOT EXIST {os.getenv("DB_NAME")};")
+            db_name = os.getenv("DB_NAME")
+
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS `{db_name}`;")
             connection.commit()
-            print(f"database : {os.getenv("DB_NAME")} is create or exixts✅")
-    except ExceptionGroup as e:
-        print("error :" ,e)
-        
+
+            print(f"Database '{db_name}' created successfully or already exists ✅")
+
+    except Exception as e:
+        print("Error:", e)
+
     finally:
         connection.close()
-        
-if __name__=="__main__":
-    create_database()        
-                    
+
+
+if __name__ == "__main__":
+    create_database()
